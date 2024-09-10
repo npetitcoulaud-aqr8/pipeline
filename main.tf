a532c829bd4f6290dafac645f29c7cabbed7af3b
Nicolas Petitcoulaud <nicolas.petitcoulaud@aqr8.fr>
2024-09-10 17:10:29 +0200

    update

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
 }
 
 resource "random_password" "password" {
-  length           = 16
+    length           = 16
   special          = true
   override_special = "!#$%&*()-_=+[]{}<>:?"
 }
